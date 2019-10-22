606e0ec41ac27ba70b64daee1f324862421bdfbc
HenriqueEichstadt <henrique98ti@gmail.com>
2019-10-22 20:05:42 -0300

    Salvando

--- trabalho1/src/trabalho1/Main.java
+++ trabalho1/src/trabalho1/Main.java
@@ -6,11 +6,12 @@ import java.util.regex.Pattern;
 public class Main {
 
     public static void main(String[] args) {
-       
+
         String pattern1 = "<(\\\"[^\\\"]*\\\"|'[^']*'|[^'\\\">])*>";
         String pattern2 = "<((?=!\\-\\-)!\\-\\-[\\s\\S]*\\-\\-|((?=\\?)\\?[\\s\\S]*\\?|((?=\\/)\\/[^.\\-\\d][^\\/\\]'\"[!#$%&()*+,;<=>?@^`{|}~ ]*|[^.\\-\\d][^\\/\\]'\"[!#$%&()*+,;<=>?@^`{|}~ ]*(?:\\s[^.\\-\\d][^\\/\\]'\"[!#$%&()*+,;<=>?@^`{|}~ ]*(?:=(?:\"[^\"]*\"|'[^']*'|[^'\"<\\s]*))?)*)\\s?\\/?))>";
-        String html = "<html><head>sdasd</head>\\<body><h1>Titulo</h1></body><footer>adasd</footer></html>";
         
+        String html = "<html><head>sdasd</head>\\<body><h1>Titulo</h1></body><footer>adasd</footer></html>";
+
         String[] vetor = html.split(pattern2);
         System.out.println("0: " + vetor[0]);
     }
