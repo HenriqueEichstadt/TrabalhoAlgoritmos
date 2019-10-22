74e9cb126a2187f6b704e2eb25e649e8057d38ab
Henrique Alexsander Eichstädt <henrique98ti@gmail.com>
2019-10-21 23:21:31 -0300

    testando engine de validação de html com expressões regulares (REGEX)

---
+++ trabalho1/src/src/ValidadorDeHtml.java
@@ -0,0 +1,15 @@
+package src;
+
+import java.util.regex.Pattern;
+
+public class ValidadorDeHtml {
+    private String htmlText;
+
+    public String getHtmlText() {
+        return htmlText;
+    }
+
+    public void setHtmlText(String htmlText) {
+        this.htmlText = htmlText;
+    }
+}
--- trabalho1/src/trabalho1/Main.java
+++ trabalho1/src/trabalho1/Main.java
@@ -1,8 +1,17 @@
 package trabalho1;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
        
+        String pattern1 = "<(\\\"[^\\\"]*\\\"|'[^']*'|[^'\\\">])*>";
+        String pattern2 = "<((?=!\\-\\-)!\\-\\-[\\s\\S]*\\-\\-|((?=\\?)\\?[\\s\\S]*\\?|((?=\\/)\\/[^.\\-\\d][^\\/\\]'\"[!#$%&()*+,;<=>?@^`{|}~ ]*|[^.\\-\\d][^\\/\\]'\"[!#$%&()*+,;<=>?@^`{|}~ ]*(?:\\s[^.\\-\\d][^\\/\\]'\"[!#$%&()*+,;<=>?@^`{|}~ ]*(?:=(?:\"[^\"]*\"|'[^']*'|[^'\"<\\s]*))?)*)\\s?\\/?))>";
+        String html = "<html><head>sdasd</head>\\<body><h1>Titulo</h1></body><footer>adasd</footer></html>";
+        
+        String[] vetor = html.split(pattern2);
+        System.out.println("0: " + vetor[0]);
     }
 }
